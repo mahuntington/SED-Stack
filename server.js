4eabd815ae2c52f105f47ca2481cc52bc9cde842
Matt Huntington <matt.huntington@gmail.com>
2016-08-13 15:21:42 -0400

    setting up public dir

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-	res.send('hello world');
-});
+app.use(express.static('public'));
 
 app.listen(PORT, function(){
 	console.log('listening on port ' + PORT);
