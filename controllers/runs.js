9a349675e8411c0f5cf442a3a820fcd5a9585a4c
Matt Huntington <matt.huntington@gmail.com>
2016-08-14 14:55:39 -0400

    run index and create routes

--- controllers/runs.js
+++ controllers/runs.js
@@ -2,7 +2,12 @@ var controller = require('express').Router();
 var runs = require('../models/run.js');
 
 controller.get('/', function(req, res){
-	res.send('oh hai!');
-})
+	res.json(runs);
+});
+
+controller.post('/', function(req, res){
+	runs.push(req.body);
+	res.json(runs);
+});
 
 module.exports = controller;
