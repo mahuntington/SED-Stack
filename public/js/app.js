337e15d7c8176c107fa9b13c9de7a06bcd7ae91b
Matt Huntington <matt.huntington@gmail.com>
2016-08-15 17:15:46 -0400

    create scales and use them in click event

--- public/js/app.js
+++ public/js/app.js
@@ -12,3 +12,28 @@ d3.select('svg').style({
 	height: HEIGHT,
 	width: WIDTH
 });
+
+
+// Create scales
+var yScale = d3.scale.linear();
+yScale.range([HEIGHT, 0]);
+yScale.domain([MIN_DISTANCE, MAX_DISTANCE]);
+
+var xScale = d3.time.scale();
+xScale.range([0, WIDTH]);
+xScale.domain([MIN_DATE, MAX_DATE]);
+
+
+// SVG Click
+d3.select('svg').on('click', function(d){
+	
+	var x = d3.event.offsetX;
+	var y = d3.event.offsetY;
+
+	var distance = yScale.invert(y);
+	var date = xScale.invert(x);
+
+	console.log("x=" + x + " date=" + date);
+	console.log("y=" + y + " distance=" + distance);
+});
+}
