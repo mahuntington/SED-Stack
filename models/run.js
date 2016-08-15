8fc47e32901fe573fd68a73450226963b9f8f603
Matt Huntington <matt.huntington@gmail.com>
2016-08-15 10:55:50 -0400

    run model

--- models/run.js
+++ models/run.js
@@ -1,1 +1,14 @@
-module.exports = [];
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('sedstack', 'matthuntington', '', {
+	host: 'localhost',
+	dialect: 'postgres'
+});
+
+var Runs = sequelize.define('run', {
+	date: Sequelize.DATE,
+	distance: Sequelize.FLOAT,
+});
+
+sequelize.sync();
+
+module.exports = Runs;
