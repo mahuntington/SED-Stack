cb643ba2ae419acd08b1613a2675c29426dc6ab9
Matt Huntington <matt.huntington@gmail.com>
2016-08-15 17:15:31 -0400

    init for min/max date/distance

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,12 @@
 var WIDTH = 800;
 var HEIGHT = 600;
 
+var MAX_DISTANCE = 5;
+var MIN_DISTANCE = 0;
+
+var MIN_DATE = new Date('2016-1-1');
+var MAX_DATE = new Date('2017-1-1');
+
 d3.select('svg').style({
 	height: HEIGHT,
 	width: WIDTH
