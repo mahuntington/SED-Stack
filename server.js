ca1676811664c4179d2dec33020a65e65f5a4e99
Matt Huntington <matt.huntington@gmail.com>
2016-08-17 15:21:31 -0400

    server uses login controller

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ app.use('/runs/', runsController);
 var usersController = require('./controllers/users.js');
 app.use('/users/', usersController);
 
+var loginsController = require('./controllers/logins.js');
+app.use('/logins/', loginsController);
+
 app.listen(PORT, function(){
 	console.log('listening on port ' + PORT);
 });
