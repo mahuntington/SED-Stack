a5c73e1964a7b912ec2beb47ab496d227d4987a8
Matt Huntington <matt.huntington@gmail.com>
2016-08-17 09:49:01 -0400

    use body parser url encoded for users

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,7 @@
 var controller = require('express').Router();
+var bodyParser = require('body-parser');
+
+controller.use(bodyParser.urlencoded({ extended: false }))
 
 controller.get('/new', function(req, res){
 	res.render('users/new.ejs');
