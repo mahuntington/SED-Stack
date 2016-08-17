9dce8ecaf1939a86a06a458398fc71f8aa9acdb5
Matt Huntington <matt.huntington@gmail.com>
2016-08-17 09:48:12 -0400

    taking body parser out of server.js

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
-app.use(bodyParser.json());
 
 var runsController = require('./controllers/runs.js');
 app.use('/runs/', runsController);
