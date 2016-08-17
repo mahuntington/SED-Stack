25591fed0ee68934f7f71abf5c66a021c04a8efc
Matt Huntington <matt.huntington@gmail.com>
2016-08-17 16:51:25 -0400

    logout functionality

--- controllers/logins.js
+++ controllers/logins.js
@@ -1,9 +1,11 @@
 var controller = require('express').Router();
 var bcrypt = require('bcrypt');
+var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 var Users = require('../models/users.js');
 
-controller.use(bodyParser.urlencoded({ extended: false }))
+controller.use(bodyParser.urlencoded({ extended: false }));
+controller.use(methodOverride('_method'));
 
 controller.get('/new', function(req, res){
 	res.render('logins/new.ejs');
@@ -18,6 +20,12 @@ controller.post('/', function(req, res){
 		if(bcrypt.compareSync(req.body.password, foundUser.password)){
 			req.session.currentUser = foundUser;
 		}
+		res.redirect('/');
+	});
+});
+
+controller.delete('/', function(req, res){
+	req.session.destroy(function(){
 		res.redirect('/');
 	});
 });
