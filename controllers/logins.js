da1709e133ad253dca67a528acbba0ac4c359046
Matt Huntington <matt.huntington@gmail.com>
2016-08-17 15:21:23 -0400

    login controller

---
+++ controllers/logins.js
@@ -0,0 +1,21 @@
+var controller = require('express').Router();
+var bodyParser = require('body-parser');
+var Users = require('../models/users.js');
+
+controller.use(bodyParser.urlencoded({ extended: false }))
+
+controller.get('/new', function(req, res){
+	res.render('logins/new.ejs');
+});
+
+controller.post('/', function(req, res){
+	Users.findOne({
+		where: {
+			username:req.body.username
+		}
+	}).then(function(foundUser){
+		res.send(foundUser);
+	});
+});
+
+module.exports = controller;
