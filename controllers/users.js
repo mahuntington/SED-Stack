13627f606199c730358cd7c1c598fe33de8da6db
Matt Huntington <matt.huntington@gmail.com>
2016-08-17 14:45:56 -0400

    encrypting password

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 var controller = require('express').Router();
 var bodyParser = require('body-parser');
+var bcrypt = require('bcrypt');
 var Users = require('../models/users.js');
 
 controller.use(bodyParser.urlencoded({ extended: false }))
@@ -9,6 +10,7 @@ controller.get('/new', function(req, res){
 });
 
 controller.post('/', function(req, res){
+	req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
 	Users.create(req.body).then(function(createdUser){
 		res.json(createdUser);
 	});;
